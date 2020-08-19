0578f70ebc0b3c823d313a6993cbca08997bc6fe
mohsinniveus <mohsin.khan@niveussolutions.com>
2020-08-19 10:25:18 +0530

    Zuul Proxiy added

---
+++ zuul-gateway-service/src/main/java/com/niveus/ZuulGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.niveus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+}
